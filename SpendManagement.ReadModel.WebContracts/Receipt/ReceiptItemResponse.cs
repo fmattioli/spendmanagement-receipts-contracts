f66b55ea0164b0b5894f02b897e5e0048f2f5069
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2024-01-05 17:52:03 -0300

    First commit

---
+++ SpendManagement.ReadModel.WebContracts/Receipt/ReceiptItemResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Web.Contracts.Receipt
+{
+    public class ReceiptItemResponse
+    {
+        public Guid Id { get; set; }
+        public Guid CategoryId { get; set; }
+        public string ItemName { get; set; } = null!;
+        public short Quantity { get; set; }
+        public decimal ItemPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string Observation { get; set; } = null!;
+    }
+}
---
+++ SpendManagement.ReadModel.WebContracts/Receipt/ReceiptResponse.cs
@@ -0,0 +1,12 @@
+﻿using Web.Contracts.Receipt;
+
+namespace SpendManagement.WebContracts.Receipt
+{
+    public class ReceiptResponse
+    {
+        public Guid Id { get; set; }
+        public string EstablishmentName { get; set; } = null!;
+        public DateTime ReceiptDate { get; set; }
+        public IEnumerable<ReceiptItemResponse> ReceiptItems { get; set; } = null!;
+    }
+}
