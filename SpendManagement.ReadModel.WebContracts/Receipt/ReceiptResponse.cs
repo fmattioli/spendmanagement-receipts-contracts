5a43d483b7ee0363c099af267a3d3616c922575c
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2024-01-07 07:51:21 -0300

    FEAT: Adding CategoryId on the Receipt

--- SpendManagement.ReadModel.WebContracts/Receipt/ReceiptResponse.cs
+++ SpendManagement.ReadModel.WebContracts/Receipt/ReceiptResponse.cs
@@ -5,6 +5,7 @@ namespace SpendManagement.WebContracts.Receipt
     public class ReceiptResponse
     {
         public Guid Id { get; set; }
+        public Guid CategoryId { get; set; }
         public string EstablishmentName { get; set; } = null!;
         public DateTime ReceiptDate { get; set; }
         public IEnumerable<ReceiptItemResponse> ReceiptItems { get; set; } = null!;
