00dafbeec5bf64c42500dbcf7d56ff27a2af576b
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2024-02-10 18:50:39 -0300

    FEAT: Creating new models

---
+++ SpendManagement.ReadModel.WebContracts/Exceptions/InternalServerErrorException.cs
@@ -0,0 +1,18 @@
+﻿namespace Web.Contracts.Exceptions
+{
+    public sealed class InternalServerErrorException : Exception
+    {
+        public InternalServerErrorException(string message)
+        {
+            Data.Add(nameof(InternalServerErrorException), message);
+        }
+
+        public InternalServerErrorException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        public InternalServerErrorException()
+        {
+        }
+    }
+}
---
+++ SpendManagement.ReadModel.WebContracts/Receipt/RecurringReceiptResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Web.Contracts.Receipt
+{
+    public class RecurringReceiptResponse
+    {
+        public Guid Id { get; set; }
+        public Guid CategoryId { get; set; }
+        public string? EstablishmentName { get; set; }
+        public DateTime DateInitialRecurrence { get; set; }
+        public DateTime DateEndRecurrence { get; set; }
+        public decimal RecurrenceTotalPrice { get; set; }
+        public string? Observation { get; set; }
+    }
+}
