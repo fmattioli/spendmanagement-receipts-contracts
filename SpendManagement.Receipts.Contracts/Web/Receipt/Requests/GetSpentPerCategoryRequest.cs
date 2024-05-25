e8fd40525f27bc1c7f6f97c2f984847bcaa1f42d
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2024-05-25 15:28:56 -0300

    FEAT: Adding new responses

---
+++ SpendManagement.Receipts.Contracts/Web/Receipt/Requests/GetSpentPerCategoryRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Contracts.Web.Receipt.Requests
+{
+    public class GetSpentPerCategoryRequest
+    {
+        public DateTime DataIni { get; set; }
+        public DateTime DataEnd { get; set; }
+    }
+}
---
+++ SpendManagement.Receipts.Contracts/Web/Receipt/Responses/SpentPerCategoryResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Contracts.Web.Receipt.Responses
+{
+    public class SpentPerCategoryResponse
+    {
+        public short Year { get; set; }
+        public short Month { get; set; }
+        public decimal TotalSpentMonthly { get; set; }
+        public List<SpentDetailResponse>? SpentDetails { get; set; }
+    }
+}
