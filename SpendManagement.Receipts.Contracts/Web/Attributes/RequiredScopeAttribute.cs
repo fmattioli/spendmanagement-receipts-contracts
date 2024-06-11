6680a9e6b33d98740cfe78eae3c273e75bae6f51
Felipe Mattioli dos Santos <felipe.mattiolidossantos@msc.com>
2024-06-11 11:21:51 +0200

    FIX: Adding back attribute

---
+++ SpendManagement.Receipts.Contracts/Web/Attributes/RequiredScopeAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Contracts.Web.Common;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Contracts.Web.Attributes
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class RequiredScopeAttribute(string scope) : Attribute, IAuthorizationFilter
+    {
+        private readonly string _scope = scope;
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = context.HttpContext.User;
+
+            var scopeClaim = user.FindFirst("scope");
+            if (scopeClaim == null || !scopeClaim.Value.Split(' ').Contains(_scope))
+            {
+                context.Result = new CustomForbidResult($"Scope {scopeClaim} not available on user {user!.Identity!.Name}.");
+            }
+        }
+    }
+}
---
+++ SpendManagement.Receipts.Contracts/Web/Common/CustomForbidResult.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Contracts.Web.Common
+{
+    public class CustomForbidResult : ObjectResult
+    {
+        public CustomForbidResult(string message) : base(new { Message = message })
+        {
+            StatusCode = StatusCodes.Status403Forbidden;
+        }
+    }
+}
