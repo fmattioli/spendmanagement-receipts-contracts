aacd07f51cd03bd8543fe4acd9bbe9080176ec4f
Felipe Mattioli dos Santos <felipe.mattiolidossantos@msc.com>
2024-07-26 11:56:10 +0200

    FIX: Rename methods to calculate discounts

--- src/SpendManagement.Receipts.Contracts/Messaging/V1/Commands/VariableReceiptCommands/CreateVariableReceiptCommand.cs
+++ src/SpendManagement.Receipts.Contracts/Messaging/V1/Commands/VariableReceiptCommands/CreateVariableReceiptCommand.cs
@@ -19,29 +19,20 @@ namespace Contracts.Messaging.V1.Commands.VariableReceiptCommands
         [DataMember(Order = 2)]
         public IEnumerable<VariableReceiptItem> ReceiptItems { get; set; } = receiptItems;
 
-        public readonly void VerifyIfItIsNecessaryApplyDiscountsBasedOnReceiptItem()
+        public readonly void ApplyDiscounts()
         {
-            var makeDiscountBasedOnReceiptItems = ReceiptItems.Any(x => x.ItemDiscount != 0.0M);
+            var checkDiscount = ReceiptItems.Any(x => x.ItemDiscount != 0.0M);
 
-            if (makeDiscountBasedOnReceiptItems)
+            if (checkDiscount)
             {
                 var totalDiscounts = ReceiptItems.Sum(x => x.ItemDiscount);
                 Receipt.Discount = totalDiscounts;
             }
         }
 
-        public readonly void ProvideDiscountOnReceiptItemTotalPrice()
+        public readonly void CalculateTotalPrice()
         {
-            foreach (var item in ReceiptItems)
-            {
-                item.TotalPrice = item.Quantity * item.ItemPrice - item.ItemDiscount;
-            }
-        }
-
-        public readonly void CalculateReceiptTotalPrice()
-        {
-            var totalReceiptWithOutDiscont = ReceiptItems.Sum(x => x.ItemPrice * x.Quantity);
-            Receipt.Total = totalReceiptWithOutDiscont - Receipt.Discount;
+            Receipt.Total = ReceiptItems.Sum(x => x.ItemPrice * x.Quantity) - Receipt.Discount;
         }
     }
 }
